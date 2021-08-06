5c4b1766cb0da5d42fefab89f20b2cff7752e93f
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-06 15:06:25 +0200

    Add Parser

--- src/main/scala/Lexer.scala
+++ src/main/scala/Lexer.scala
@@ -6,19 +6,6 @@ import scala.util.matching.Regex
 import scala.util.parsing.combinator.RegexParsers
 
 
-// Grammar
-// Symbols in backticks are terminals / literals
-// ###################################################################
-// Exp      ::= Atom | UnOpExp | BinOpExp | (Exp)
-// Atom     ::= [a-z]
-// UnOpExp  ::= `not` Exp
-// BinOpExp ::= Exp `and` Exp | Exp `or` Exp | Exp `implies` Exp
-// ###################################################################
-// Operator precedence (from strongest to weakest)
-// 1. Unary operator `not` binds the strongest
-// 2. Binary Operators `and` and `or` (left associative)
-// 3. Binary Operator `implies` (right associative)
-
 object Lexer extends RegexParsers {
   override def skipWhitespace: Boolean = true
 
---
+++ src/main/scala/Parser.scala
@@ -0,0 +1,26 @@
+package io.jaschdoc
+
+import Tokens.LogicToken
+
+import scala.util.parsing.combinator.Parsers
+
+
+// Grammar
+// Symbols in backticks are terminals / literals
+// ###################################################################
+// Exp      ::= Atom | UnOpExp | BinOpExp | (Exp)
+// Atom     ::= [a-z]
+// UnOpExp  ::= `not` Exp
+// BinOpExp ::= Exp `and` Exp | Exp `or` Exp | Exp `implies` Exp
+// ###################################################################
+// Operator precedence (from strongest to weakest)
+// 1. Unary operator `not` binds the strongest
+// 2. Binary Operators `and` and `or` (left associative)
+// 3. Binary Operator `implies` (right associative)
+
+
+object Parser extends Parsers {
+
+  override type Elem = LogicToken
+
+}
