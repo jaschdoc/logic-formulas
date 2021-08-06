aa09cdc9671075ccc45c16a6bd20285680a17f2e
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-06 15:04:46 +0200

    Test Lexer

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,9 +4,14 @@ import Ast.{AndExp, AtomExp}
 
 object Main extends App {
 
-  val formula = AndExp(AtomExp(true), AtomExp(false))
-  val v = Interpreter.eval(formula)
+  val formulaExp = AndExp(AtomExp(true), AtomExp(false))
+  val v = Interpreter.eval(formulaExp)
   println(v)
+
+  val formula = "p and q -> f"
+  val malformedFormula = "p and q <_ f"
+  println(Lexer(formula))
+  println(Lexer(malformedFormula))
 
 }
 
