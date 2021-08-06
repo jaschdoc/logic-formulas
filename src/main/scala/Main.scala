4a7867385895026d4eae4355bf10974848b659b3
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-06 16:00:05 +0200

    Clean Code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,8 @@
 package io.jaschdoc
 
-import Ast.{AndExp, AtomExp}
 
 object Main extends App {
 
-  val formulaExp = AndExp(AtomExp(true), AtomExp(false))
-  val v = Interpreter.eval(formulaExp)
-  println(v)
 
   val formula = "p and q -> f"
   val malformedFormula = "p and q <_ f"
