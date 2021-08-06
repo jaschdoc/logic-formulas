72c05db3dff1a7ca9a8a5d64b137e6f38f4063b9
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-06 15:43:07 +0200

    Update Interpreter

--- src/main/scala/Interpreter.scala
+++ src/main/scala/Interpreter.scala
@@ -5,11 +5,24 @@ import Ast._
 object Interpreter {
 
   type Value = Boolean
+  type Env = Map[Id, Value]
 
-  def eval(e: Exp): Value = e match {
-    case AtomExp(p) => p
-    case AndExp(p, q) => eval(p) && eval(q)
-    case OrExp(p, q) => eval(p) || eval(q)
-    case ImplicationExp(p, q) => !eval(p) || eval(q)
+  def eval(e: Exp, env: Env): Value = e match {
+    case AtomExp(p) => env.getOrElse(p, throw new InterpreterError("Internal Error!"))
+    case UnOpExp(op, p) =>
+      val p1 = eval(p, env)
+      op match {
+        case Not => !p1
+      }
+    case BinOpExp(p, op, q) =>
+      val p1 = eval(p, env)
+      val q1 = eval(q, env)
+      op match {
+        case And => p1 && q1
+        case Or => p1 || q1
+        case Implication => !p1 || q1
+      }
   }
+
+  class InterpreterError(msg: String) extends FormulaError(msg)
 }
