cbea700a8ea0b2545370aadc5c181b9c42c71954
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-06 23:58:00 +0200

    Evaluate Input

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,10 +11,7 @@ object Main extends App {
   println(lexed)
   val program = Parser.parse(lexed)
   println(program)
-  val results = Environment.allPossibleFrom(program)
-  println(freeVars(program))
-    //.map(env => (env, Interpreter.eval(program, env)))
+  val results = Environment.allPossibleFrom(program).map(env => (env, Interpreter.eval(program, env)))
   println(results)
-  println(results.size)
 }
 
