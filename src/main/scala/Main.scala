99ebf3a648f56e7003dc6d130c650e27f1812da1
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-07 22:04:44 +0200

    Implement CLI

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,11 @@
 package io.jaschdoc
 
 object Main extends App {
+  val welcomeMsg =
+    """
+      |Welcome to the logic formula tool!
+      |
+      |""".stripMargin
+  println(welcomeMsg)
 
 }
-
