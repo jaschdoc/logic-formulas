4d4014d59b01924ceaecf24c205dd673bb3279eb
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-07 19:41:48 +0200

    Add Tests

--- src/test/scala/ParserSpec.scala
+++ src/test/scala/ParserSpec.scala
@@ -1,6 +1,6 @@
 package io.jaschdoc
 
-import Ast.AtomExp
+import Ast._
 import syntax.Parser
 import syntax.Parser.FormulaParserError
 
@@ -13,8 +13,22 @@ class ParserSpec extends UnitSpec {
   }
 
   it should "accept a single atom" in {
-    assertResult(AtomExp("p")) {
+    val expected = AtomExp("p")
+    assertResult(expected) {
       Parser.parse("p")
     }
   }
+
+  ignore should "not accept two atoms only" in {
+    Parser.parse("p q")
+    fail("Not implemented yet")
+  }
+
+  it should "recognize a negation expression" in {
+    val expected = UnOpExp(NotUnOp, AtomExp("p"))
+    assertResult(expected) {
+      Parser.parse("not p")
+    }
+  }
+
 }
