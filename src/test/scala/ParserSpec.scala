9a447e96d84b395876cf62f7f98e47acb02673e1
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-07 18:47:17 +0200

    Clean Code

--- src/test/scala/ParserSpec.scala
+++ src/test/scala/ParserSpec.scala
@@ -4,6 +4,7 @@ import syntax.Parser
 import syntax.Parser.FormulaParserError
 
 class ParserSpec extends UnitSpec {
+
   "The parser" should "not accept an empty program" in {
     assertThrows[FormulaParserError] {
       Parser.parse("")
