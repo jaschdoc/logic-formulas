a7e586ff80dfb21d181bf09d0f3a25a1e83d3e6f
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-07 22:26:18 +0200

    Add Help Message

--- src/main/scala/Commands.scala
+++ src/main/scala/Commands.scala
@@ -4,9 +4,20 @@ object Commands {
 
   def from(args: Array[String]): Set[Command] = ???
 
-  def allCommands: Set[]
+  def allCommands: Set[Command] = Set(AllCombinations, Help)
 
   sealed trait Command {
+
+    override def toString: String =
+      s"""$toCommand\t${
+        val a = aliases.mkString(", ")
+        if (a.isEmpty) ""
+        else if (aliases.size == 1) "Alias: " + a
+        else "Aliases: " + a
+      }
+         |\t  $description
+         |""".stripMargin
+
     def toCommand: String
 
     def aliases: Set[String]
@@ -24,5 +35,18 @@ object Commands {
     override def description: String = "Checks all combinations of truth values for all variables in a formula (SAT-solver)."
 
     override def run: String = ???
+  }
+
+  case object Help extends Command {
+    override def toCommand: String = "help"
+
+    override def aliases: Set[String] = Set()
+
+    override def description: String = "Shows this help message."
+
+    override def run: String =
+      s"""Commands:
+         |${Commands.allCommands.map(c => "\t" + c.toString).mkString("\n")}
+         |""".stripMargin
   }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,10 +2,15 @@ package io.jaschdoc
 
 object Main extends App {
   val welcomeMsg =
-    """
+    s"""
       |Welcome to the logic formula tool!
-      |
+      |Here are your options:
+      |${Commands.Help.run}
       |""".stripMargin
+
   println(welcomeMsg)
 
+  println()
+
+  println("We should now prompt you for some input")
 }
