9ff6d0f03ada21014254215554098284b2ada215
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-07 21:38:43 +0200

    Clean Code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,43 +1,6 @@
 package io.jaschdoc
 
-import Ast._
-import syntax.Parser
-
 object Main extends App {
-  val input = "p and q -> f"
-  val program = Parser.parse(input)
-  println(program)
-  val results = Environment.allPossibleFrom(program).map(env => (env, Interpreter.eval(program, env)))
-  println(results)
-  println()
 
-  //println(Parser.parse("p q"))
-  println()
-
-  val e = BinOpExp(
-    AtomExp("p"),
-    AndBinOp,
-    BinOpExp(
-      UnOpExp(
-        NotUnOp,
-        AtomExp("q")
-      ),
-      OrBinOp,
-      BinOpExp(
-        AtomExp("s"),
-        ImplicationBinOp,
-        UnOpExp(
-          NotUnOp,
-          BinOpExp(
-            AtomExp("a"),
-            OrBinOp,
-            AtomExp("b")
-          )
-        )
-      )
-    )
-  )
-
-  println(Unparser.unparse(e))
 }
 
