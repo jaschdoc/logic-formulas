f531b8ceb121cb1cdb817b47275ded63d4009354
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-07 20:58:25 +0200

    Simplify Message

--- src/main/scala/syntax/SyntaxError.scala
+++ src/main/scala/syntax/SyntaxError.scala
@@ -1,6 +1,6 @@
 package io.jaschdoc
 package syntax
 
-class SyntaxError(msg: String, input: String = "") extends FormulaCompilationError(s"$msg" + (if (input.nonEmpty) s"\nInput was: $input" else "")) {
-  override def toString: String = s"$msg" + (if (input.nonEmpty) s"\nInput was: $input" else "")
+class SyntaxError(msg: String, input: String = "") extends FormulaCompilationError(s"$msg - Input was: $input") {
+  override def toString: String = s"$msg - Input was: $input"
 }
