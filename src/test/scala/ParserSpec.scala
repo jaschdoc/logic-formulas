c57c651ba7d47fbcd9be3fe73de6af3b048c8f69
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-07 18:45:48 +0200

    Add ParserSpec

---
+++ src/test/scala/ParserSpec.scala
@@ -0,0 +1,12 @@
+package io.jaschdoc
+
+import syntax.Parser
+import syntax.Parser.FormulaParserError
+
+class ParserSpec extends UnitSpec {
+  "The parser" should "not accept an empty program" in {
+    assertThrows[FormulaParserError] {
+      Parser.parse("")
+    }
+  }
+}
