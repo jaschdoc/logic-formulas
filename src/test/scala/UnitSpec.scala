ccd995d7fbe9a8144fac51d18ea950abcd4bdaba
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-07 21:41:10 +0200

    Clean Code

--- src/test/scala/UnitSpec.scala
+++ src/test/scala/UnitSpec.scala
@@ -1,7 +1,7 @@
 package io.jaschdoc
 
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.{must, should}
+import org.scalatest.matchers.should
 import org.scalatest.{Inside, Inspectors, OptionValues}
 
 abstract class UnitSpec extends AnyFlatSpec with should.Matchers
