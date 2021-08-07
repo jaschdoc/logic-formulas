06baac3609e4946f3a9546812017bc335cd58dc3
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-07 21:25:20 +0200

    Refactor Tests

--- src/test/scala/InterpreterSuite.scala
+++ src/test/scala/InterpreterSuite.scala
@@ -5,18 +5,20 @@ import syntax.Parser
 class InterpreterSuite extends UnitSpec {
 
   "The Interpreter" must "evaluate true to true" in {
-    assertResult(true) {
-      val exp = Parser.parse("p")
-      val env = Map("p" -> true)
-      Interpreter.eval(exp, env)
-    }
+    val exp = Parser.parse("p")
+    val env = Map("p" -> true)
+    Interpreter.eval(exp, env) should equal(true)
   }
 
-  "The Interpreter" must "evaluate false to false" in {
-    assertResult(false) {
-      val exp = Parser.parse("p")
-      val env = Map("p" -> false)
-      Interpreter.eval(exp, env)
-    }
+  it must "evaluate false to false" in {
+    val exp = Parser.parse("p")
+    val env = Map("p" -> false)
+    Interpreter.eval(exp, env) should equal(false)
+  }
+
+  it must "adhere to the truth table of the NOT operator (1)" in {
+    val exp = Parser.parse("not p")
+    val env = Map("p" -> true)
+    Interpreter.eval(exp, env) should equal(false)
   }
 }
