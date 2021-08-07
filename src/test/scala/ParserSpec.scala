1fcecbfa831ac7ffb3d488e98f9530f42c61007f
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-07 21:10:04 +0200

    Add Tests

--- src/test/scala/ParserSpec.scala
+++ src/test/scala/ParserSpec.scala
@@ -172,12 +172,19 @@ class ParserSpec extends UnitSpec {
       )
     )
 
-    val actual = Parser.parse("p and (not q or (s -> not (a or b)))\n")
+    val actual = Parser.parse("p and (not q or (s -> not (a or b)))")
 
     withClue(Unparser.unparse(actual)) {
       assertResult(expected) {
         actual
       }
+    }
+  }
+
+  it should "not be sensitive to whitespaces" in {
+    val expected = BinOpExp(AtomExp("p"), AndBinOp, AtomExp("q"))
+    assertResult(expected) {
+      Parser.parse("     p     and       q        ")
     }
   }
 }
