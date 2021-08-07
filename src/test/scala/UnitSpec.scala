878e631e5c9faaf725d4c6556ce27279d1cf1e44
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-07 18:40:23 +0200

    Add Base Class

---
+++ src/test/scala/UnitSpec.scala
@@ -0,0 +1,8 @@
+package io.jaschdoc
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+import org.scalatest.{Inside, Inspectors, OptionValues}
+
+abstract class UnitSpec extends AnyFlatSpec with should.Matchers
+  with OptionValues with Inside with Inspectors
