d33e861b8cd2132d0c4b5357a0c131c4ba93888d
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-07 21:27:39 +0200

    Add Tests

--- src/test/scala/InterpreterSuite.scala
+++ src/test/scala/InterpreterSuite.scala
@@ -21,4 +21,58 @@ class InterpreterSuite extends UnitSpec {
     val env = Map("p" -> true)
     Interpreter.eval(exp, env) should equal(false)
   }
+
+  it must "adhere to the truth table of the NOT operator (2)" in {
+    val exp = Parser.parse("not p")
+    val env = Map("p" -> false)
+    Interpreter.eval(exp, env) should equal(true)
+  }
+
+  it must "adhere to the truth table of the AND operator (1)" in {
+    val exp = Parser.parse("p and q")
+    val env = Map("p" -> true, "q" -> true)
+    Interpreter.eval(exp, env) should equal(true)
+  }
+
+  it must "adhere to the truth table of the AND operator (2)" in {
+    val exp = Parser.parse("p and q")
+    val env = Map("p" -> true, "q" -> false)
+    Interpreter.eval(exp, env) should equal(false)
+  }
+
+  it must "adhere to the truth table of the AND operator (3)" in {
+    val exp = Parser.parse("p and q")
+    val env = Map("p" -> false, "q" -> true)
+    Interpreter.eval(exp, env) should equal(false)
+  }
+
+  it must "adhere to the truth table of the AND operator (4)" in {
+    val exp = Parser.parse("p and q")
+    val env = Map("p" -> false, "q" -> false)
+    Interpreter.eval(exp, env) should equal(false)
+  }
+
+  it must "adhere to the truth table of the OR operator (1)" in {
+    val exp = Parser.parse("p or q")
+    val env = Map("p" -> true, "q" -> true)
+    Interpreter.eval(exp, env) should equal(true)
+  }
+
+  it must "adhere to the truth table of the OR operator (2)" in {
+    val exp = Parser.parse("p or q")
+    val env = Map("p" -> true, "q" -> false)
+    Interpreter.eval(exp, env) should equal(true)
+  }
+
+  it must "adhere to the truth table of the OR operator (3)" in {
+    val exp = Parser.parse("p or q")
+    val env = Map("p" -> false, "q" -> true)
+    Interpreter.eval(exp, env) should equal(true)
+  }
+
+  it must "adhere to the truth table of the OR operator (4)" in {
+    val exp = Parser.parse("p or q")
+    val env = Map("p" -> false, "q" -> false)
+    Interpreter.eval(exp, env) should equal(false)
+  }
 }
