bb93d7b8c241d2d4988315a9e9d615ba5886b014
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-07 21:30:44 +0200

    Refactor Tests

--- src/test/scala/InterpreterSuite.scala
+++ src/test/scala/InterpreterSuite.scala
@@ -7,72 +7,72 @@ class InterpreterSuite extends UnitSpec {
   "The Interpreter" must "evaluate true to true" in {
     val exp = Parser.parse("p")
     val env = Map("p" -> true)
-    Interpreter.eval(exp, env) should equal(true)
+    Interpreter.eval(exp, env) mustBe true
   }
 
   it must "evaluate false to false" in {
     val exp = Parser.parse("p")
     val env = Map("p" -> false)
-    Interpreter.eval(exp, env) should equal(false)
+    Interpreter.eval(exp, env) mustBe false
   }
 
   it must "adhere to the truth table of the NOT operator (1)" in {
     val exp = Parser.parse("not p")
     val env = Map("p" -> true)
-    Interpreter.eval(exp, env) should equal(false)
+    Interpreter.eval(exp, env) mustBe false
   }
 
   it must "adhere to the truth table of the NOT operator (2)" in {
     val exp = Parser.parse("not p")
     val env = Map("p" -> false)
-    Interpreter.eval(exp, env) should equal(true)
+    Interpreter.eval(exp, env) mustBe true
   }
 
   it must "adhere to the truth table of the AND operator (1)" in {
     val exp = Parser.parse("p and q")
     val env = Map("p" -> true, "q" -> true)
-    Interpreter.eval(exp, env) should equal(true)
+    Interpreter.eval(exp, env) mustBe true
   }
 
   it must "adhere to the truth table of the AND operator (2)" in {
     val exp = Parser.parse("p and q")
     val env = Map("p" -> true, "q" -> false)
-    Interpreter.eval(exp, env) should equal(false)
+    Interpreter.eval(exp, env) mustBe false
   }
 
   it must "adhere to the truth table of the AND operator (3)" in {
     val exp = Parser.parse("p and q")
     val env = Map("p" -> false, "q" -> true)
-    Interpreter.eval(exp, env) should equal(false)
+    Interpreter.eval(exp, env) mustBe false
   }
 
   it must "adhere to the truth table of the AND operator (4)" in {
     val exp = Parser.parse("p and q")
     val env = Map("p" -> false, "q" -> false)
-    Interpreter.eval(exp, env) should equal(false)
+    Interpreter.eval(exp, env) mustBe false
   }
 
   it must "adhere to the truth table of the OR operator (1)" in {
     val exp = Parser.parse("p or q")
     val env = Map("p" -> true, "q" -> true)
-    Interpreter.eval(exp, env) should equal(true)
+    Interpreter.eval(exp, env) mustBe true
   }
 
   it must "adhere to the truth table of the OR operator (2)" in {
     val exp = Parser.parse("p or q")
     val env = Map("p" -> true, "q" -> false)
-    Interpreter.eval(exp, env) should equal(true)
+    Interpreter.eval(exp, env) mustBe true
   }
 
   it must "adhere to the truth table of the OR operator (3)" in {
     val exp = Parser.parse("p or q")
     val env = Map("p" -> false, "q" -> true)
-    Interpreter.eval(exp, env) should equal(true)
+    Interpreter.eval(exp, env) mustBe true
   }
 
   it must "adhere to the truth table of the OR operator (4)" in {
     val exp = Parser.parse("p or q")
     val env = Map("p" -> false, "q" -> false)
-    Interpreter.eval(exp, env) should equal(false)
+    Interpreter.eval(exp, env) mustBe false
   }
 }
--- src/test/scala/UnitSpec.scala
+++ src/test/scala/UnitSpec.scala
@@ -1,8 +1,8 @@
 package io.jaschdoc
 
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should
+import org.scalatest.matchers.{must, should}
 import org.scalatest.{Inside, Inspectors, OptionValues}
 
 abstract class UnitSpec extends AnyFlatSpec with should.Matchers
-  with OptionValues with Inside with Inspectors
+  with must.Matchers with OptionValues with Inside with Inspectors
