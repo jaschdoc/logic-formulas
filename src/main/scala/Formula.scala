22b171c58890606b236a1c1cf9c22b469edbdd43
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2021-08-12 14:53:00 +0200

    Add Scala DSL

--- src/main/scala/Formula.scala
+++ src/main/scala/Formula.scala
@@ -10,7 +10,19 @@ trait Formula {
 
   def not(e: Exp): Exp = UnOpExp(NotUnOp, e)
 
-  implicit class ProtoString(string: String) {
-    def and(right: Exp): Exp = BinOpExp(string, AndBinOp, right)
+  sealed trait ProtoExp {
+    def and(right: Exp): Exp
+
+    def or(right: Exp): Exp
+
+    def ~>(right: Exp): Exp
+  }
+
+  implicit class ProtoString(string: String) extends ProtoExp {
+    override def and(right: Exp): Exp = BinOpExp(string, AndBinOp, right)
+
+    override def or(right: Exp): Exp = BinOpExp(string, OrBinOp, right)
+
+    override def ~>(right: Exp): Exp = BinOpExp(string, ImplicationBinOp, right)
   }
 }
--- src/test/scala/ScalaDSLSuite.scala
+++ src/test/scala/ScalaDSLSuite.scala
@@ -8,19 +8,47 @@ class ScalaDSLSuite extends UnitSpec {
     def toExp: Exp
   }
 
-  "The Scala DSL" should "allow for atomic expressions 'p'" in {
+  "The Scala DSL" should "allow for atomic expressions \"p\"" in {
     case object TestFormula extends TestFormula {
       override def toExp: Exp = "p"
     }
-
     TestFormula.toExp shouldBe AtomExp("p")
   }
 
-  it should "convert 'p' and 'q' to a BinOpExp" in {
+  it should "convert '\"p\" and \"q\"' to a BinOpExp" in {
     case object TestFormula extends TestFormula {
       override def toExp: Exp = "p" and "q"
     }
+    val expectedAst = BinOpExp(AtomExp("p"), AndBinOp, AtomExp("q"))
 
-    TestFormula.toExp shouldBe BinOpExp(AtomExp("p"), AndBinOp, AtomExp("q"))
+    TestFormula.toExp shouldBe expectedAst
   }
+
+  it should "convert '\"p\" or \"q\"' to a BinOpExp" in {
+    case object TestFormula extends TestFormula {
+      override def toExp: Exp = "p" or "q"
+    }
+    val expectedAst = BinOpExp(AtomExp("p"), OrBinOp, AtomExp("q"))
+
+    TestFormula.toExp shouldBe expectedAst
+  }
+
+  it should "convert '\"p\" ~> \"q\"' to a BinOpExp" in {
+    case object TestFormula extends TestFormula {
+      override def toExp: Exp = "p" ~> "q"
+    }
+    val expectedAst = BinOpExp(AtomExp("p"), ImplicationBinOp, AtomExp("q"))
+
+    TestFormula.toExp shouldBe expectedAst
+  }
+
+  it should "convert 'not \"p\"' to an UnOpExp" in {
+    case object TestFormula extends TestFormula {
+      override def toExp: Exp = not("p")
+    }
+    val expectedAst = UnOpExp(NotUnOp, AtomExp("p"))
+
+    TestFormula.toExp shouldBe expectedAst
+  }
+
 }
